c6d8242953ce8039200f5744683abbb8e9209a07
Virnaliz Cruz <40121477+garezana@users.noreply.github.com>
2021-12-03 13:54:12 -0500

    WIP R update
    Functions to read and download CRAN mac and win html pages and find when pages were last changed or modified.

--- carpenpi/updates/r_html_downloader.py
+++ carpenpi/updates/r_html_downloader.py
@@ -21,14 +21,31 @@ def parse_dateline_string(string):
         string_lower_match=lowercase_line
         #date_find(string_lower_match)
 
-
-
-def file_search(filenamepattern): 
-  for filename in glob.glob(filenamepattern):
+#Search for file name and returns date
+def file_search(filenamepattern,path): 
+  oldinfo=[]
+  oldfileinfodic={"currentfilepath":[],"currentfiledate":[],"currentfileostype":[]};
+  #keys = ["currentfilepath", "currentfiledate"]
+  #oldfileinfodic= dict.fromkeys(keys)
+  oldinfo=[]
+  for filename in glob.glob(path + "r_*_html_*.txt"):
     if os.path.isfile(filename):
       olddate=filename.split("_")[-1].split(".")[0]
-      return(olddate)
+      oldinfo.extend([[filename, olddate, filename.split("_")[1]]])
 
+      #Using dic of lists
+      #oldfileinfodic["currentfilepath"].append(filename)
+      #oldfileinfodic["currentfiledate"].append(olddate)
+      #oldfileinfodic["currentfileostype"].append(filename.split("_")[1])
+      #print (oldfileinfodic)
+      #oldfileinfodic.update({'currentfilepath':filename, "currentfiledate": olddate });
+
+       #dates_dict[key].append(date)
+  #return (oldfileinfodic)
+  return (oldinfo)
+
+
+#Find,parse and return date in online file
 def date_find(textline):
   date_match = re.search(r'(\d{4})[/.-](\d{2})[/.-](\d{2})', textline) # Search for string that matches yyyy/mm/dd or yyyy-mm-dd format.
   for fmt in ('%Y-%m-%d', '%Y/%m/%d'): # Change date to string.
@@ -39,31 +56,27 @@ def date_find(textline):
     except:
         print("")
 
+#Download and read html from URL
 if __name__ == "__main__":
-  currentdate= file_search("r_*_html_*.txt")
+  cwd=os.path.dirname(__file__)+"/"
+  currentfileinfo= file_search("r_*_html_*.txt", cwd ) #Passing pattern and dir
+  #print(currentfileinfo)
   #file_web_downloads(".txt")
-
+  count=0
   for url in url_os:
+
     #file_web_downloads(url)
     urlfile = urllib.request.urlopen(url)
-   #print(urlfile)
     for line in urlfile:
-      decoded = line.decode("utf-8")
-      #print(decoded)
+      decoded = line.decode("utf-8") 
       lowercase_line = decoded.lower()
-      #print(lowercase_line)
       if last_change_string in lowercase_line or last_modified_string in lowercase_line:
         string_lower_match=lowercase_line
-        print(string_lower_match)
-        print(currentdate)
         newdate=date_find(string_lower_match)
-        #print( newdate,currentdate)
-        #if newdate == currentdate:
-          #print(newdate,currentdate)
-          
-    # Change file name.
-    #new_file = url_filename[url] + "_" + date_string1 + ".txt"
-    #os.rename(url_filename[url] + ".txt", new_file)
+        new_file = cwd+ url_filename[url] + "_" + newdate + ".txt"
+        if not(os.path.exists(new_file)):
+          urllib.request.urlretrieve(url, new_file)
+        count += 1
 
 
 # Create a method in which you can send the web file name, local file name
