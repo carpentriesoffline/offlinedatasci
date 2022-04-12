1600a2211f360d5c7fe74eabe6698af584d863ee
Virnaliz Cruz <virnaliz.cruz@weecology.org>
2022-04-12 07:11:25 -0400

    adding bs4 to install_requires

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['wget'],  # Optional
+    install_requires=['wget',"beautifulsoup4"],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
