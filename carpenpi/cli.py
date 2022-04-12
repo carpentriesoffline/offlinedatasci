a0bee57558503436cae2cd21bdc6f4ac7a3865f0
Ethan White <ethan@weecology.org>
2022-04-12 14:16:19 -0400

    Start shifting CLI to a single setup command
    * Rename download* to setup*
    * Use a single command with args to control what is setup
    
    General structure is complete but commands need to be added for most
    arguments.
    
    Co-authored-by: Virnaliz Cruz <virnaliz.cruz@weecology.org>

--- carpenpi/cli.py
+++ carpenpi/cli.py
@@ -1,40 +1,42 @@
-import os
-import sys
-
 import argparse
-from carpenpi import download, download_lessons, downloadfiles
-from carpenpi import create_carpenpi_dir
-import carpenpi
+from carpenpi import *
 
 def get_parser():
     parser = argparse.ArgumentParser(prog='carpenpi')
     subparsers = parser.add_subparsers(dest='command')
 
-    download_parser = subparsers.add_parser('download',
-                                            help = 'Download CarpenPi content')
-    download_parser.add_argument('path',
+    setup_parser = subparsers.add_parser('setup',
+                                            help = 'Setup CarpenPi content')
+    setup_parser.add_argument('selection',
+                              metavar='selection',
+                              type=str,
+                              help="Selection of components to setup. Use 'all' to setup everything.",
+                              default='all'
+                              )
+    setup_parser.add_argument('path',
                                 metavar='path',
                                 type=str,
-                                help='path to download carpenpi files to')
-    download_parser = subparsers.add_parser('download-lessons',
-                                            help = 'Download Carpentries lessons')
-    download_parser.add_argument('path',
-                                metavar='path',
-                                type=str,
-                                help='path to download carpenpi files to')
+                                help='path to setup carpenpi files in')
     return parser
 
 def main():
     parser = get_parser()
     args = parser.parse_args()
     print(args)
-    if args.command == 'download':
+    if args.command == 'setup':
         carpenpi_dir = create_carpenpi_dir(args.path)
-        download(carpenpi_dir)
-    if args.command == 'download-lessons':
-        carpenpi_dir = create_carpenpi_dir(args.path)
-        download_lessons.download_lessons(carpenpi_dir)
-
+        if args.selection == 'all':
+            pass
+        elif args.selection == 'r':
+            pass
+        elif args.selection == 'rstudio':
+            download_Rstudio(carpenpi_dir)
+        elif args.selection == 'cran':
+            pass
+        elif args.selection == 'lessons':
+            pass
+        else:
+            print("That option is not available. Choose from: all, r, rstudio, cran, lessons")
 
 if __name__ == '__main__':
     main()
