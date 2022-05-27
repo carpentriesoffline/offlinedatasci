65247c465a9a84d9c8e2fde8c4bc6f6628203032
Virnaliz Cruz <virnaliz.cruz@weecology.org>
2022-05-27 13:59:09 -0400

    WIP minor help fixes

--- offlinedatasci/cli.py
+++ offlinedatasci/cli.py
@@ -4,14 +4,16 @@ from offlinedatasci import *
 
 def main():
     parser = argparse.ArgumentParser(prog='offlinedatasci')
-    INSTALL_OPTIONS=["all", "custom","lessons","minicran","python", "python_libraries", "r","rstudio"]
+    INSTALL_OPTIONS=["all", "custom", "lessons", "minicran", "python", "python_libraries", "r", "rstudio"]
 
     p=parser.add_argument("-i",
                         dest= "install",
-                        nargs='?', 
-                        const='all',
+                        nargs=1, 
+                        #const='all',
                         choices=INSTALL_OPTIONS,
-                        help="Specify install options: all, custom, lessons,minicran,python, python_libraries,r,rstudio")
+                        required=True,
+                        help=f"Specify install options: {INSTALL_OPTIONS.join(', ')}"
+                        )
 
     parser.add_argument("--python",
                         nargs='+', 
@@ -27,7 +29,7 @@ def main():
     #                    type=str, 
     #                    choices=["carpentries","data Science", "custom"])
 
-    parser.add_argument('-path',
+    parser.add_argument('--path',
                         metavar='path',
                         type=str,
                         required=True,
@@ -36,21 +38,18 @@ def main():
     args, unknown = parser.parse_known_args()
 
     ods_dir = get_ods_dir(args.path)
-    print(ods_dir)
 
     def get_installer_functions(selection,ods_dir):
-        print(selection)
         if selection=="all":
-            print("Doing all functions")
             download_r(ods_dir)
             download_software(ods_dir, "rstudio")
             download_minicran(ods_dir) 
             #download_lessons(ods_dir)
             download_software(ods_dir,"python")
             download_python_libraries(ods_dir)
+
         elif selection=="rstudio" or selection=="python":
-            print( selection," downloading")
-            download_software(ods_dir,selection)
+            download_software(ods_dir, selection)
         else:
             try:
                 download_function = f"download_{selection}"
@@ -58,9 +57,8 @@ def main():
             except Exception:
                 print(f'method does not exist for selection {selection}')
 
-    get_installer_functions(args.install,ods_dir)
+    get_installer_functions(args.install[0],ods_dir)
 
-   
 if __name__ == '__main__':
-                            
+              
     main()
